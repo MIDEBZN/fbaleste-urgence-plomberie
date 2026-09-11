3aeebda0dae00c03525664d80517c729cce00d25
Fbaleste Plomberie <contact@fbaleste-plomberie.be>
2026-09-11 20:34:41 +0200

    Fix SEO and canonicalization issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.urgenceplomberie.be',
+  trailingSlash: 'always',
   integrations: [
     sitemap({
       changefreq: 'weekly',
